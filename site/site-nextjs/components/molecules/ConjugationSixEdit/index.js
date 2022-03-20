706a30ce6ef534c7ac61d8f22591adfcfbcb9075
Vincenzo <vincenzorm117@gmail.com>
2022-03-20 15:04:50 -0400

    Setup conjugationsixedit component

--- site/site-nextjs/components/molecules/ConjugationSixEdit/index.js
+++ site/site-nextjs/components/molecules/ConjugationSixEdit/index.js
@@ -2,11 +2,12 @@ import { useCallback, useEffect, useRef } from "react";
 import styles from "./styles.module.scss";
 import get from "lodash/get";
 
-export default function ConjugationSixEdit({
-  title,
+export default function ConjugationSixFlashcard({
+  title = null,
   verbRef,
   keyPrefix,
   className = "",
+  disabledFields = null,
 }) {
   const inputRef = {
     io: useRef(),
@@ -15,6 +16,15 @@ export default function ConjugationSixEdit({
     noi: useRef(),
     voi: useRef(),
     loro: useRef(),
+  };
+
+  const disabled = {
+    io: get(disabledFields, "io", false),
+    tu: get(disabledFields, "tu", false),
+    lei: get(disabledFields, "lei", false),
+    noi: get(disabledFields, "noi", false),
+    voi: get(disabledFields, "voi", false),
+    loro: get(disabledFields, "loro", false),
   };
 
   const update = useCallback(
@@ -32,54 +42,76 @@ export default function ConjugationSixEdit({
 
   return (
     <div className={className}>
-      <h2 className={styles.title}>{title}</h2>
+      {title && <h2 className={styles.title}>{title}</h2>}
       <div className={styles.values}>
-        <div className={styles.value} data-pronoun="io">
+        <div className={styles.value} data-pronoun="io" disabled={disabled.io}>
           <input
             ref={inputRef.io}
             type="text"
             className={styles.input}
             onChange={() => update("io")}
+            disabled={disabled.io}
           />
         </div>
-        <div className={styles.value} data-pronoun="noi">
+        <div
+          className={styles.value}
+          data-pronoun="noi"
+          disabled={disabled.noi}
+        >
           <input
             ref={inputRef.noi}
             type="text"
             className={styles.input}
             onChange={() => update("noi")}
+            disabled={disabled.noi}
           />
         </div>
-        <div className={styles.value} data-pronoun="tu">
+        <div className={styles.value} data-pronoun="tu" disabled={disabled.tu}>
           <input
             ref={inputRef.tu}
             type="text"
             className={styles.input}
             onChange={() => update("tu")}
+            disabled={disabled.tu}
           />
         </div>
-        <div className={styles.value} data-pronoun="voi">
+        <div
+          className={styles.value}
+          data-pronoun="voi"
+          disabled={disabled.voi}
+        >
           <input
             ref={inputRef.voi}
             type="text"
             className={styles.input}
             onChange={() => update("voi")}
+            disabled={disabled.voi}
           />
         </div>
-        <div className={styles.value} data-pronoun="lui lei">
+        <div
+          className={styles.value}
+          data-pronoun="lui lei"
+          disabled={disabled.lei}
+        >
           <input
             ref={inputRef.lei}
             type="text"
             className={styles.input}
             onChange={() => update("lei")}
+            disabled={disabled.lei}
           />
         </div>
-        <div className={styles.value} data-pronoun="loro">
+        <div
+          className={styles.value}
+          data-pronoun="loro"
+          disabled={disabled.loro}
+        >
           <input
             ref={inputRef.loro}
             type="text"
             className={styles.input}
             onChange={() => update("loro")}
+            disabled={disabled.loro}
           />
         </div>
       </div>
