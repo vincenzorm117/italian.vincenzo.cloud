c05e1930054a4a2b81c9fe550be197100782bf01
Vincenzo <vincenzorm117@gmail.com>
2022-03-20 15:03:37 -0400

    Moved graphql client to its own file

--- site/site-nextjs/pages/_app.js
+++ site/site-nextjs/pages/_app.js
@@ -1,14 +1,10 @@
 import "../styles/globals.scss";
 import "tailwindcss/tailwind.css";
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "../services/graphql";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  const client = new ApolloClient({
-    uri: "http://italian.vincenzo.local:4000/graphql",
-    cache: new InMemoryCache({ addTypename: false }),
-  });
-
   return (
     <ApolloProvider client={client}>
       <Component {...pageProps} />
--- site/site-nextjs/services/graphql.js
+++ site/site-nextjs/services/graphql.js
@@ -1,8 +1,6 @@
-// import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-// const client = new ApolloClient({
-//   uri: "http://italian.vincenzo.local:4000/graphql",
-//   cache: new InMemoryCache(),
-// });
-
-// export default ;
+export const client = new ApolloClient({
+  uri: `${process.env.SERVER_ORIGIN}/graphql`,
+  cache: new InMemoryCache({ addTypename: false }),
+});
