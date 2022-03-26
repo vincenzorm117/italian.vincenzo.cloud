b564a9220b41996ae92ff93fa49affa1c9b88cff
Vincenzo <vincenzorm117@gmail.com>
2022-03-26 16:24:58 -0400

    Added noun resolver

--- server/js-server/src/graphql/resolvers/_index.js
+++ server/js-server/src/graphql/resolvers/_index.js
@@ -1,6 +1,9 @@
 module.exports = {
+    // Verbs
     verbs: require('./verbs/multiple'),
     verb: require('./verbs/single'),
     updateVerb: require('./verbs/update'),
     randomVerbs: require('./verbs/random'),
+    // Nouns
+    noun: require('./nouns/single'),
 }
---
+++ server/js-server/src/graphql/resolvers/nouns/single.js
@@ -0,0 +1,28 @@
+const { connection } = require('../../../mysql')
+
+module.exports = (gqlArgs, req, gqlParams) =>
+    new Promise((resolve, reject) => {
+        const { selections } = gqlParams.fieldNodes[0].selectionSet
+        const sqlFields = selections.map(s => s.name.value)
+
+        // [Step] Construct query
+        let query = `SELECT ${sqlFields.join(', ')} FROM Nouns`
+        const sqlParams = []
+
+        if (gqlArgs.hasOwnProperty('id')) {
+            query += ' WHERE id = ?'
+            sqlParams.push(gqlArgs.id)
+        }
+
+        query += ' LIMIT 1'
+
+        console.log(query)
+
+        connection
+            .query(query, sqlParams, function (error, results, fields) {
+                resolve(results[0])
+            })
+            .on('error', error => {
+                reject(error)
+            })
+    })
